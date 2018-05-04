ea86f170dab9466701db5e74900420c74bca2445
calok <c.alok@hotmail.com>
2018-05-04 10:27:23 -0400

    Create UpdateGitRepo.ps1
    Initial commit

---
+++ UpdateGitRepo.ps1
@@ -0,0 +1,9 @@
+$git = "C:\Program Files\Git\cmd\git";
+if (!$git) {
+  Write-Host "Looks like you don't have git executable installed or it's in a different location. 
+    Please enter the path to git.cmd or git.exe:";
+  $git = Read-Host;
+  Set-Variable -Name git -Value $git;
+}
+ls -name -Exclude *.* | foreach {cd .\$_ ; .$git pull upstream master; .$git push origin master; cd ..}
+Write-Host "Done.";
